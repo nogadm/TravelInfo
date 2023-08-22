c8453d8b55b694ca8f7e5cedc4231d520c17d9b8
nogadm <nogadm@gmail.com>
2023-08-22 12:39:01 +0300

    fixed weather function

--- weather_api_request.py
+++ weather_api_request.py
@@ -4,7 +4,7 @@ import pandas as pd
 key = "a45ea9764ada4fa6b5675219232108"
 
 
-def get_weather_forcast(city, start_date, end_date, degree_type):
+def get_weather_forcast(city, start_date, end_date):
     # find the date range between the first and last day
     date_range = pd.date_range(start=start_date, end=end_date)
 
@@ -17,24 +17,24 @@ def get_weather_forcast(city, start_date, end_date, degree_type):
         response = requests.get(f"http://api.weatherapi.com/v1/future.json?key={key}&q={city}&dt={date_str}")
         date_weather_dict = response.json()
 
-        if degree_type == "C":
-            # get min temperature and max temperature in celsius
-            min_temp = date_weather_dict["forecast"]["forecastday"][0]["day"]["mintemp_c"]
-            max_temp = date_weather_dict["forecast"]["forecastday"][0]["day"]["maxtemp_c"]
-        else:
-            # get min temperature and max temperature in fahrenheit
-            min_temp = date_weather_dict["forecast"]["forecastday"][0]["day"]["mintemp_f"]
-            max_temp = date_weather_dict["forecast"]["forecastday"][0]["day"]["maxtemp_f"]
+        # get min temperature and max temperature in celsius
+        min_temp_c = date_weather_dict["forecast"]["forecastday"][0]["day"]["mintemp_c"]
+        max_temp_c = date_weather_dict["forecast"]["forecastday"][0]["day"]["maxtemp_c"]
+
+        # get min temperature and max temperature in fahrenheit
+        min_temp_f = date_weather_dict["forecast"]["forecastday"][0]["day"]["mintemp_f"]
+        max_temp_f = date_weather_dict["forecast"]["forecastday"][0]["day"]["maxtemp_f"]
 
         # get sunrise time and sunset time
         sunrise = date_weather_dict["forecast"]["forecastday"][0]["astro"]["sunrise"]
         sunset = date_weather_dict["forecast"]["forecastday"][0]["astro"]["sunset"]
 
         # save details in dictionary
-        forcast[date_str] = {"min_temp":min_temp, "max_temp":max_temp,
+        forcast[date_str] = {"min_temp_c":min_temp_c, "max_temp_c":max_temp_c,
+                             "min_temp_f": min_temp_f, "max_temp_f": max_temp_f,
                              "sunrise":sunrise, "sunset":sunset}
 
     return forcast
 
 
-print(get_weather_forcast("New York City", "2024-04-21", "2024-04-23", "C"))
+print(get_weather_forcast("New York City", "2024-04-21", "2024-04-23"))
