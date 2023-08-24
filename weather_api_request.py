16f0aafebda572f3eafd0767bbd1b3db9dfcad39
nogadm <nogadm@gmail.com>
2023-08-24 16:45:52 +0300

    added logic in case departure date is earlier than arrival date

--- weather_api_request.py
+++ weather_api_request.py
@@ -9,34 +9,35 @@ key = "a45ea9764ada4fa6b5675219232108"
 def get_weather_forcast(city, start_date, end_date):
     # find the date range between the first and last day
     date_range = pd.date_range(start=start_date, end=end_date)
-
     # dictionary that will contain data on each day in the range
-    forcast = dict()
+    forecast = dict()
 
-    # for each day in the range, send an API request to Weather API
-    for date in date_range:
-        date_str = date.strftime('%Y-%m-%d')
-        response = requests.get(f"http://api.weatherapi.com/v1/future.json?key={key}&q={city}&dt={date_str}")
-        date_weather_dict = response.json()
+    if end_date < start_date:
+        forecast["Departure date is earlier than arrival date, please check the dates."] = ""
+    else:
+        # for each day in the range, send an API request to Weather API
+        for date in date_range:
+            date_str = date.strftime('%Y-%m-%d')
+            response = requests.get(f"http://api.weatherapi.com/v1/future.json?key={key}&q={city}&dt={date_str}")
+            date_weather_dict = response.json()
 
-        # get min temperature and max temperature in celsius
-        min_temp_c = date_weather_dict["forecast"]["forecastday"][0]["day"]["mintemp_c"]
-        max_temp_c = date_weather_dict["forecast"]["forecastday"][0]["day"]["maxtemp_c"]
+            # get min temperature and max temperature in celsius
+            min_temp_c = date_weather_dict["forecast"]["forecastday"][0]["day"]["mintemp_c"]
+            max_temp_c = date_weather_dict["forecast"]["forecastday"][0]["day"]["maxtemp_c"]
 
-        # get min temperature and max temperature in fahrenheit
-        min_temp_f = date_weather_dict["forecast"]["forecastday"][0]["day"]["mintemp_f"]
-        max_temp_f = date_weather_dict["forecast"]["forecastday"][0]["day"]["maxtemp_f"]
+            # get min temperature and max temperature in fahrenheit
+            min_temp_f = date_weather_dict["forecast"]["forecastday"][0]["day"]["mintemp_f"]
+            max_temp_f = date_weather_dict["forecast"]["forecastday"][0]["day"]["maxtemp_f"]
 
-        # get sunrise time and sunset time
-        sunrise = date_weather_dict["forecast"]["forecastday"][0]["astro"]["sunrise"]
-        sunset = date_weather_dict["forecast"]["forecastday"][0]["astro"]["sunset"]
+            # get sunrise time and sunset time
+            sunrise = date_weather_dict["forecast"]["forecastday"][0]["astro"]["sunrise"]
+            sunset = date_weather_dict["forecast"]["forecastday"][0]["astro"]["sunset"]
 
-        # save details in dictionary
-        forcast[date_str] = {"min_temp_c":min_temp_c, "max_temp_c":max_temp_c,
-                             "min_temp_f": min_temp_f, "max_temp_f": max_temp_f,
-                             "sunrise":sunrise, "sunset":sunset}
-
-    return forcast
+            # save details in dictionary
+            forecast[date_str] = {"min_temp_c":min_temp_c, "max_temp_c":max_temp_c,
+                                 "min_temp_f": min_temp_f, "max_temp_f": max_temp_f,
+                                 "sunrise":sunrise, "sunset":sunset}
+    return forecast
 
 
 def weather_icon():
